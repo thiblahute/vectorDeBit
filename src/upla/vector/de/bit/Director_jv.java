8119f73a798b401e7780f8e64bf8aa794aafb3bf
DaveX <davex@DaveX-Mobile.(none)>
2009-11-09 20:50:07 -0300

    Add _jv

---
+++ src/upla/vector/de/bit/Director_jv.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package upla.vector.de.bit;
+
+/**
+ *
+ * @author 
+ */
+public class Director_jv implements Director {
+
+    Director_jv();
+    
+}
