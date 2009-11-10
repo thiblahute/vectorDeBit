0f738ca589396dece51ffc30adfc592760752e90
Thibault Saunier <saunierthibault@gmail.com>
2009-11-09 21:04:46 -0300

    Add VectorBit_jv.java

---
+++ src/upla/vector/de/bit/VectorBit_jv.java
@@ -0,0 +1,25 @@
+/**
+  @ Project : Vector de bit
+  @ File Name : VectorBit_jv.java
+  @ Date : 02/11/2009
+  @ Author : David Vargas; Thibault Saunier, Carlos Lagos
+*/
+
+package upla.vector.de.bit;
+
+
+/** */
+public class VectorBit_jv implements VectorBit
+{
+	/** */
+	public VectorBit buildVectorBit()
+	{
+        
+	}
+	
+	/** */
+	public VectorBit buildVectorBit(VectorBit val)
+	{
+              
+	}
+}
