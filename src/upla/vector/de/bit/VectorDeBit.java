b08e8ce04ee44116cdeef68b09b29e12e5fe6ca0
DaveX <davex@DaveX-Mobile.(none)>
2009-11-15 22:06:06 -0300

    faltaba autor a una class

--- src/upla/vector/de/bit/VectorDeBit.java
+++ src/upla/vector/de/bit/VectorDeBit.java
@@ -1,4 +1,13 @@
+/**
+  @ Project : Vector de bit
+  @ File Name : Builder_VectorBit.java
+  @ Date : 02/11/2009
+  @ Author : David Vargas; Thibault Saunier, Carlos Lagos
+*/
+
 package upla.vector.de.bit;
+
+/** @public class  Vector Bits*/
 
 public class VectorDeBit
 { 
